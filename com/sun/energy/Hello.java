272b6f348c5012e1cc374b1261ad62faf68d3725
unknown <gorakh.phadtare@gmail.com>
2020-05-21 17:58:18 +0530

    AGN-1001-Second Chekin

--- com/sun/energy/Hello.java
+++ com/sun/energy/Hello.java
@@ -1,8 +1,8 @@
-package com.sun.energy;
+package com.sun.energy
 public class Hello
 {
 public static void main (String args[])
 {
-System.out.println("Java Hello World");
+System.out.println("Java Hello World1111111111");
 }
 }
